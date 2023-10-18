4d6314d989661d0e7ee4ccdf0a7e27e8f42b27ef
OsvaldoPrat <148344180+OsvaldoPrat@users.noreply.github.com>
2023-10-18 08:52:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Altera o texto do título
+document.querySelector("h1").textContent = "Programação e Sistemas de Informação"
+document.querySelector("footer").innerHTML += `
+<a href="https://github.com/bard/explorando-o-mundo-tecnologico">GitHub</a>
